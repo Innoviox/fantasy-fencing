3ee30411397dab62aa05eceabd956f566fcb7c8a
Simon Chervenak <sarras305@gmail.com>
2020-12-12 18:19:50 -0500

    Update api.py

--- api/api.py
+++ api/api.py
@@ -9,3 +9,7 @@ api.include_router(fencers.router, prefix="/fencers", tags=["users"])
 @api.get("/")
 async def root():
     return {"message": "Hello Bigger Applications!"}
+
+@api.get("/{id}")
+async def get_fencer_by_id(id):
+    ...
